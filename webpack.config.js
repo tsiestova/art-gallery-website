c391538efc6972b416862814d52500538224a919
Tetiana Siestova <siestova.t@gmail.com>
2023-03-01 04:56:38 +0200

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-// const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV == "production";
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
